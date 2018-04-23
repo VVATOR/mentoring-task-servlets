f764fdebd4dc3fb684e1963e7c092065688a7f43
Vikhliayeu, Vitali <Vitali.Vikhliayeu@wolterskluwer.com>
2018-04-23 18:05:01 +0300

    add servlet

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # mentoring-task-servlets 
 description: TODO
 
-## BUILD
+## STEP 1: BUILD
+
 ```
 	mvn clean package
 ```
 
-## INSTALL
+## STEP 2: INSTALL
+
+After previous step in folder *target* 
+you can find file with name *TODO.war* 
+
+this file you nessesary need  copy in folder deploy of *Apache Tomcat* 
+
+
+after that you need run *Apache Tomcat*.
 
 TODO
--- src/main/java/com/epam/mentoring/MyServlet.java
+++ src/main/java/com/epam/mentoring/MyServlet.java
@@ -14,18 +14,39 @@ import javax.servlet.http.HttpServletResponse;
 public class MyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public MyServlet() {
-        // TODO Auto-generated constructor stub
-    }
-
 	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 * Default constructor.
 	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public MyServlet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		// super.doDelete(req, resp);
+		resp.getWriter().append("delete ok");
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// super.doGet(req, resp);
+		resp.getWriter().append("get ok");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// super.doPost(req, resp);
+		resp.getWriter().append("post ok");
+	}
+
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// super.doPut(req, resp);
+		resp.getWriter().append("put ok");
 	}
 
 }
